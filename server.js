cd47c4e5c775adbbf7453c2e6225f8311c27b0c6
Mark Oosterveld <mark@oosterveld.org>
2017-06-23 07:31:29 -0400

    more

--- server.js
+++ server.js
@@ -7,9 +7,67 @@ const views = require('koa-views');
 const fs = require('fs');
 const url = require('url');
 
-const index = fs.readFileSync('./public/index.html', {encoding: 'utf8'});
+// const index = fs.readFileSync('./public/index.html', {encoding: 'utf8'});
 
 const app = new Koa();
+
+function render() {
+    let amount = '100';
+    let exchanged = '125';
+
+    return `<!DOCTYPE html>
+<html>
+    <head>
+    </head>
+    <body>
+        <h2>OANDA</h2>
+        <h3>Currency Converter</h3>
+
+        <form name="form" action="/blah" method="get">
+            <div>Current Ammount</div>
+            <input name="amount" value="${amount}" />
+            <select name="source">
+                <option>AUD</option>
+                <option>GBP</option>
+                <option>CAD</option>
+                <option selected>EUR</option>
+                <option>INR</option>
+                <option>JPY</option>
+                <option>SGD</option>
+                <option>CHF</option>
+                <option>USD</option>
+            </select>
+            <br />
+            <input type="submit" name="submit" value="Switch" />
+            <div>Exchanged Amount</div>
+            <input disabled value="${exchanged}" />
+            <select name="destination">
+                <option>AUD</option>
+                <option>GBP</option>
+                <option>CAD</option>
+                <option>EUR</option>
+                <option>INR</option>
+                <option>JPY</option>
+                <option>SGD</option>
+                <option>CHF</option>
+                <option>USD</option>
+            </select>
+            <br />
+            <div>Apply Specific Rate</div>
+            <select name="rate">
+                <option value="0">+/- 0% Interbank Rate</option>
+                <option value="1">+/- 1%</option>
+                <option value="2">+/- 2% Typical ATM Rate</option>
+                <option value="3">+/- 3% Typical Credit Card Rate</option>
+                <option value="4">+/- 4%</option>
+                <option value="5">+/- 5% Typical Kiosk Rate</option>
+            </select>
+            <br />
+            <input type="submit" name="submit" value="Submit" />
+        </form>
+    </body>
+</html>`;
+}
 
 // x-response-time
 // app.use(async function (ctx, next) {
@@ -36,12 +94,13 @@ app.use(async (ctx, next) => {
 // Router
 app.use(async (ctx, next) => {
     const x = url.parse(ctx.request.href, true);
+    const qs = x.query;
 
-    console.log('request', JSON.stringify(ctx.request));
+    console.log('request', JSON.stringify(qs));
 
-    console.log('url', JSON.stringify(x));
-    console.log('qs', ctx.request.querystring);
-    console.log('path', ctx.request.path);
+    // console.log('url', JSON.stringify(x));
+    // console.log('qs', ctx.request.querystring);
+    // console.log('path', ctx.request.path);
 
     await next();
 });
@@ -55,7 +114,7 @@ app.use(ctx => {
 //     console.log('path', ctx.request.path);
 
     ctx.response.status = 200;
-    ctx.response.body = index;
+    ctx.response.body = render();
 })
 
 http.createServer(app.callback()).listen(3000);
